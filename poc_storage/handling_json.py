9391d9adeec0afc8c725b4408247033cf3aa527a
andy gg <andy.gg@somemail.com>
2018-07-23 19:00:25 +0200

    made the static realtiv urls platform independent. Changed backslashes to forward slashes

--- poc_storage/handling_json.py
+++ poc_storage/handling_json.py
@@ -7,24 +7,22 @@ base_path = Path(__file__).parents[2]
 
 def load_form_contents():
     with open(
-            '{}\\smoking_eval\\content\\form-content.json'.format(base_path),
+            '{}/smoking_eval/content/form-content.json'.format(base_path),
             encoding="utf-8"
     ) as json_data:
         return json.load(json_data)
 
 
-def load_dictionary(relative_path="\\smoking_eval\\store\\questionaries.json"):
+def load_dictionary(relative_path="/smoking_eval/store/questionaries.json"):
     with open(
-            # '{}\\smoking_eval\\store\\questionaries.json'.format(base_path),
             '{}{}'.format(base_path, relative_path),
             encoding="utf-8"
     ) as json_data:
         return json.load(json_data)
 
 
-def dump_dictionary(eval_dictionary, relative_path="\\smoking_eval\\store\\questionaries.json"):
+def dump_dictionary(eval_dictionary, relative_path="/smoking_eval/store/questionaries.json"):
     with open(
-            # '{}\\smoking_eval\\store\\questionaries.json'.format(base_path),
             '{}{}'.format(base_path, relative_path),
             'w',
             encoding="utf-8"
