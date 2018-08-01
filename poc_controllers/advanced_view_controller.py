dcdc93f104508cc0b9bbcb0634292ab960f672cd
andy gg <andy.gg@somemail.com>
2018-08-01 14:24:18 +0200

    added post form and save to questinary functionality

--- poc_controllers/advanced_view_controller.py
+++ poc_controllers/advanced_view_controller.py
@@ -1,6 +1,9 @@
 import tornado.web
+from tornado.web import MissingArgumentError
 
 from poc_services.advanced_view_service import build_questions_tree
+from poc_storage.handling_json import load_form_contents
+from poc_services.questionary_service import update_questionary
 
 
 class AdvancedViewController(tornado.web.RequestHandler):
@@ -15,20 +18,29 @@ class AdvancedViewController(tornado.web.RequestHandler):
             question_tree=build_questions_tree()
         )
 
-    # def post(self, *args, **kwargs):
-    #     pass
-    # answer = self.get_body_argument(name="answer")
-    #
-    # update_questionary(
-    #     key=self.get_body_argument(name="question_code"),
-    #     value=answer
-    # )
-    #
-    # if answer == "Ja":
-    #     redirection = self.get_body_argument(name="next_yes")
-    # elif answer == "Nein":
-    #     redirection = self.get_body_argument(name="next_no")
-    # else:
-    #     redirection = "/home"
-    #
-    # self.redirect("{}".format(redirection))
+    def post(self, *args, **kwargs):
+        all_questions = load_form_contents()
+
+        question_answer_pairs = []
+        for current_question in all_questions["codes"]:
+            try:
+                question_answer_pairs.append(
+                    {
+                        "question": current_question["code"],
+                        "answer": self.get_body_argument("answer-{}".format(
+                            str(current_question["code"])
+                        ))
+                    }
+                )
+            except MissingArgumentError:
+                continue
+
+        for pair in question_answer_pairs:
+            if pair["answer"]:
+                update_questionary(
+                    key=pair["question"],
+                    value=pair["answer"]
+                )
+
+        redirection = "/acquisition"
+        self.redirect("{}".format(redirection))
